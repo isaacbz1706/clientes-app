3424a198e7ca3871cbd9469df92f3c218dd5e5de
isaacbz1706 <zamora.becerra99@gmail.com>
2023-06-16 21:24:25 -0600

    template

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html', //aqui se puede poner codigo entre comillas simples de html
+  template: `<h1>prueba</h1>`, //aqui se puede poner codigo entre comillas simples de html con (``) 
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
