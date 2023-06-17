b4bf3965423ac0caa521aa118d73ae5dd2b38bd9
isaacbz1706 <zamora.becerra99@gmail.com>
2023-06-16 20:46:34 -0600

    Componente

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html', //aqui se puede poner codigo entre comillas simples de html
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
