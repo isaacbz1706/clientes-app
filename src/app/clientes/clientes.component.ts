75430648161e419c4ad19f236254af9e637a3b47
isaacbz1706 <zamora.becerra99@gmail.com>
2023-06-22 15:18:56 -0600

    insertar listado de datos con archivo json

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,27 +1,15 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit, importProvidersFrom } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.jsaon';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit{
-Clientes : Cliente[] = [{id:1, nombre:'Isaac', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:2, nombre:'lalo', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:3, nombre:'yos', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:4, nombre:'yamilet', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:5, nombre:'jesus', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:6, nombre:'celia', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:7, nombre:'david', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:8, nombre:'rosa', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:9, nombre:'rodrigo', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:10, nombre:'shak', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
-{id:11, nombre:'lili', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'}
-
-
-
-
-];
+clientes : Cliente[] = [];
 constructor(){}
-  ngOnInit(){}
+  ngOnInit(){
+    this.clientes=CLIENTES;
+  }
 }
---
+++ src/app/clientes/clientes.jsaon.ts
@@ -0,0 +1,14 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES : Cliente[] = [{id:1, nombre:'Isaac', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:2, nombre:'lalo', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:3, nombre:'yos', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:4, nombre:'yamilet', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:5, nombre:'jesus', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:6, nombre:'celia', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:7, nombre:'david', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:8, nombre:'rosa', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:9, nombre:'rodrigo', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:10, nombre:'shak', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'},
+{id:11, nombre:'lili', apellido:'Becerra Zamora', email:'zamora.becerra99@gmail.com', createAt:'1998-17-06'}
+];
